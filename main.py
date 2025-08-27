48b86f1c87ead1f85a5e149eef622988e3925edd
valerie00230322 <230322@studierende.htl-donaustadt.at>
2025-08-27 19:14:36 +0200

    changed kiwi version to 1.1.1 --> due to display problems with MDSnackbar

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from kivy.clock import Clock
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import Snackbar
 import threading
 import time
 import json
-
+import pymysql
 
 class CocktailLogic:
     def __init__(self, cocktails):
         self.cocktails = cocktails
 
     def mix_cocktail(self, name, finished_callback):
-        """Startet den Mixvorgang in einem Thread."""
+        "Startet den Mixvorgang in einem Thread."
         thread = threading.Thread(target=self._simulate_mixing, args=(name, finished_callback), daemon=True)
         thread.start()
 
     def _simulate_mixing(self, name, finished_callback):
-        """Simuliert das Pumpen der Zutaten."""
+        "Simuliert das Pumpen der Zutaten."
         zutaten = self.cocktails[name]
         for zutat, sekunden in zutaten.items():
             print(f"[SIM] Pumpe {zutat} für {sekunden} Sekunden.")
@@ -42,7 +42,7 @@ class CocktailApp(MDApp):
             self.cocktails = json.load(f)
 
     def on_start(self):
-        """Füllt die Liste mit Cocktails."""
+      #"Füllt die Liste mit Cocktails."
         for name in self.cocktails.keys():
             self.root.ids.cocktail_list.add_widget(
                 self.create_list_item(name)
@@ -60,9 +60,18 @@ class CocktailApp(MDApp):
         self.logic.mix_cocktail(name, self.show_finished_snackbar)
 
     def show_finished_snackbar(self, name):
-        snackbar = MDSnackbar(text=f"{name} ist fertig!")
+        snackbar = Snackbar(text=f"{name} ist fertig!")
         snackbar.open()
 
 
 if __name__ == "__main__":
-    CocktailApp().run()
+   CocktailApp().run()
+   """  conn = pymysql.connect(
+       host="172.21.104.81",  # z. B. 172.20.200.45
+       user="cocktailuser",
+       password="test123",
+       database="cocktaildb" 
+   )"""
+
+   """print("Verbindung erfolgreich:", conn.open)
+   conn.close()"""
